aad79f9453973c903c98d7b0757d60f06f5e5206
h0nyk <82706840+h0nyk@users.noreply.github.com>
2025-06-23 17:24:22 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ echo Creating environment and installing Chatterbox-TTS-WebUI...
 python -m venv myenv
 call myenv\Scripts\activate.bat
 pip install chatterbox-tts gradio
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
 
 echo Done! Run run_tts.bat 
 pause
